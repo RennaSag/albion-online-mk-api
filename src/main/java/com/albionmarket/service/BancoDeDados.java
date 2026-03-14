a1edfcbc131d03701f598ef224b6b0a2e1bbe4f1
Renna Sag <rennasamuel_2003@hotmail.com>
2026-03-14 00:33:46 -0300

    Armas, armaduras de todoso os materiais completos

--- src/main/java/com/albionmarket/service/BancoDeDados.java
+++ src/main/java/com/albionmarket/service/BancoDeDados.java
@@ -9,10 +9,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//sintaxe:
+//item("", "", ""),
+
 /**
  *
  * AS SUBCATEGORIAS JA PREENCHIDAS ESTÃO PRONTAS, FALTA AS OUTRAS CATEGORIAS E SUBCATEGORIAS DE ITENS DO JOGO
- *
+ * <p>
  * catalogo completo de itens, categorias e cidades do Albion Online.
  * Nomes em portugues br
  * IDs baseados na Albion Online Data API (west.albion-online-data.com).
@@ -128,12 +131,20 @@ public class BancoDeDados {
                 )),
 
                 new Subcategoria("Luvas de Guerra", Arrays.asList(
+                        item("2H_KNUCKLES_SET", "Luvas de Lutador", "luvas de lutador"),
+                        item("2H_KNUCKLES_SET2", "Braçadeiras de Batalha", " bracadeiras de batalha"),
+                        item("2H_KNUCKLES_SET3", "Manoplas Claravas", "manoplas cravadas"),
+                        item("2H_KNUCKLES_KEEPER", "Luvas Ursinas", "luvas ursinas"),
+                        item("2H_KNUCKLES_HELL", "Mãos Infernais", "maos infernais"),
+                        item("2H_KNUCKLES_MORGANA", "Cestus Golpeadores", "cestus golpeadores"),
+                        item("2H_KNUCKLES_AVALON", "Punhos de Avalon", "punhos de avalon"),
+                        item("2H_KNUCKLES_CRYSTAL", "Braçadeiras Pulsantes", "bracadeiras pulsantes")
 
                 ))
         )));
 
         // cajados
-        lista.add(new Categoria("", "Cajados Arcano", Arrays.asList(
+        lista.add(new Categoria("", "Cajados", Arrays.asList(
                 new Subcategoria("Cajado Arcano", Arrays.asList(
                         item("MAIN_ARCANESTAFF", "Cajado Arcano", "cajado arcano arcane"),
                         item("2H_ARCANESTAFF", "Cajado Arcano Elevado", "cajado arcano elevado"),
@@ -156,6 +167,15 @@ public class BancoDeDados {
                 )),
 
                 new Subcategoria("Cajado de Gelo", Arrays.asList(
+                        item("MAIN_FROSTSTAFF", "Cajado de Gelo", "cajado de gelo"),
+                        item("2H_FROSTSTAFF", "Cajado de Gelo Elevado", "cajado de gelo elevado"),
+                        item("2H_GLACIALSTAFF", "Cajado Glacial", "cajado glacial"),
+                        item("MAIN_FROSTSTAFF_KEEPER", "Cajado Enregelante", "cajado enregelante"),
+                        item("2H_ICEGAUNTLETS_HELL", "Cajado de Sincelo", "cajado sincelo"),
+                        item("2H_ICECRYSTAL_UNDEAD", "Prisma Geleterno", "primas geleterno"),
+                        item("MAIN_FROSTSTAFF_AVALON", "Uivo Frio", "uivo frio"),
+                        item("2H_FROSTSTAFF_CRYSTAL", "Cajado Ártico", "cajado artico")
+
 
                 )),
 
@@ -171,72 +191,185 @@ public class BancoDeDados {
                 )),
 
                 new Subcategoria("Cajado Sagrado", Arrays.asList(
-
+                        item("MAIN_HOLYSTAFF", "Cajado Sagrado", "cajado sagrado"),
+                        item("2H_HOLYSTAFF", "Cajado Sagrado Elevado", "cajado sagrado elevado"),
+                        item("2H_DIVINESTAFF", "Cajado Divino", "cajado divino"),
+                        item("MAIN_HOLYSTAFF_MORGANA", "Cajado Avivador", "cajado avivador"),
+                        item("2H_HOLYSTAFF_HELL", "Cajado Corrompido", "cajado corrompido"),
+                        item("2H_HOLYSTAFF_UNDEAD", "Cajado da Redenção", "cajado da redencao"),
+                        item("MAIN_HOLYSTAFF_AVALON", "Queda Santa", "queda santa"),
+                        item("2H_HOLYSTAFF_CRYSTAL", "Cajado Exaltado", "cajado exaltado")
                 )),
 
                 new Subcategoria("Cajado da Natureza", Arrays.asList(
+                        item("MAIN_NATURESTAFF", "Cajado da Natureza", "cajado da natureza"),
+                        item("2H_NATURESTAFF", "Cajado da Natureza Elevado", "cajado da natureza elevado"),
+                        item("2H_WILDSTAFF", "Cajado Selvagem", "cajado selvagem"),
+                        item("MAIN_NATURESTAFF_KEEPER", "Cajado Druídico", "cajado druidico"),
+                        item("2H_NATURESTAFF_HELL", "Cajado Pustulento", "cajado pustulento"),
+                        item("2H_NATURESTAFF_KEEPER", "Cajado Rampante", "cajado rampante"),
+                        item("MAIN_NATURESTAFF_AVALON", "Raiz Férrea", "raiz ferrea"),
+                        item("MAIN_NATURESTAFF_CRYSTAL", "Cajado de Crosta Forjada", "cajado de crosta forjada")
+
 
                 )),
 
                 new Subcategoria("Arco", Arrays.asList(
+                        item("2H_BOW", "Arco", "arco"),
+                        item("2H_WARBOW", "Arco de Guerra", "arco de guerra"),
+                        item("2H_LONGBOW", "Arco Longo", "arco longo"),
+                        item("2H_LONGBOW_UNDEAD", "Arco Sussurrante", "arco sussurante"),
+                        item("2H_BOW_HELL", "Arco Plangente", "arco plangente"),
+                        item("2H_BOW_KEEPER", "Arco Badônico", "arco badonico"),
+                        item("2H_BOW_AVALON", "Fura-bruma", "fura bruma"),
+                        item("2H_BOW_CRYSTAL", "Arco do Andarilho Celeste", "arco do andarilho celeste")
 
                 )),
+
+
                 new Subcategoria("Besta", Arrays.asList(
+                        item("2H_CROSSBOW", "Besta", "besta"),
+                        item("2H_CROSSBOWLARGE", "Besta Pesada", "besta pesada"),
+                        item("MAIN_1HCROSSBOW", "Besta Leve", "besta leve"),
+                        item("2H_REPEATINGCROSSBOW_UNDEAD", "Repetidor Lamentoso", "repetidor lamentoso"),
+                        item("2H_DUALCROSSBOW_HELL", "Lança-virotes", "lança virotes"),
+                        item("2H_CROSSBOWLARGE_MORGANA", "Arco de Cerco", "arco de certo"),
+                        item("2H_CROSSBOW_CANNON_AVALON", "Modelador de Energia", "modelador de energia"),
+                        item("2H_DUALCROSSBOW_CRYSTAL", "Detonadores Reluzentes", "detonadores reluzentes")
 
                 )),
                 new Subcategoria("Cajado Metamordo", Arrays.asList(
-
+                        item("2H_SHAPESHIFTER_SET1", "Cajado de Predador", "cajado de predador"),
+                        item("2H_SHAPESHIFTER_SET2", "Cajado Enraizado", "cajado enraizado"),
+                        item("2H_SHAPESHIFTER_SET3", "Cajado Primitivo", "cajado primitivo"),
+                        item("2H_SHAPESHIFTER_MORGANA", "Cajado da Lua de Sangue", "cajado da lua de sangue"),
+                        item("2H_SHAPESHIFTER_HELL", "Cajado Endemoniado", "cajado endemoniado"),
+                        item("2H_SHAPESHIFTER_KEEPER", "Cajado Rúnico da Terra", "cajado runico da terra"),
+                        item("2H_SHAPESHIFTER_AVALON", "Cajado Invocador da Luz", "cajado invocador da luz"),
+                        item("2H_SHAPESHIFTER_CRYSTAL", "Cajado Petrificante", "cajado petrificante")
                 ))
 
 
         )));
 
 
-        // elmos, capotes e chapeus
-        lista.add(new Categoria("", "Armadura de Capacete", Arrays.asList(
+        // elmos, capotes e capuz
+        lista.add(new Categoria("", "Elmos, Capotes e Capuz", Arrays.asList(
 
-                new Subcategoria("Tecido (Mago)", Arrays.asList(
-
+                new Subcategoria("Capote de Tecido", Arrays.asList(
+                        item("HEAD_CLOTH_SET1", "Capote de Erudito", "capote de erudito"),
+                        item("HEAD_CLOTH_SET2", "Capote de Clérigo", "capote de clerigo"),
+                        item("HEAD_CLOTH_SET3", "Capote de Mago", "capote de mago"),
+                        item("HEAD_CLOTH_KEEPER", "Capote de Druida", "capote de druida"),
+                        item("HEAD_CLOTH_HELL", "Capote Malévolo", "capote de malevolo"),
+                        item("HEAD_CLOTH_MORGANA", "Capote Sectário", "capote de sectario"),
+                        item("HEAD_CLOTH_FEY", "Capote Feérico", "capote feerico"),
+                        item("HEAD_CLOTH_AVALON", "Capote da Pureza", "capote da pureza"),
+                        item("HEAD_CLOTH_ROYAL", "Capote Real", "capote real")
                 )),
 
-                new Subcategoria("Elmos de couro", Arrays.asList(
-
+                new Subcategoria("Capuz de couro", Arrays.asList(
+                        item("HEAD_LEATHER_SET1", "Capuz de Mercenário", "Capuz de mercenario"),
+                        item("HEAD_LEATHER_SET2", "Capuz de Caçador", "Capuz de cacador caçador"),
+                        item("HEAD_LEATHER_SET3", "Capuz de Assassino", "Capuz de assassino"),
+                        item("HEAD_LEATHER_MORGANA", "Capuz de Espreitador", "Capuz de espreitador"),
+                        item("HEAD_LEATHER_HELL", "Capuz Inferial", "Capuz inferial"),
+                        item("HEAD_LEATHER_UNDEAD", "Capuz Espectral", "Capuz espectral"),
+                        item("HEAD_LEATHER_FEY", "Capuz de Andarilho da Névoa", "Capuz de andarilho da nevoa"),
+                        item("EAD_LEATHER_AVALON", "Capuz da Tenacidade", "Capuz da tenacidade")
                 )),
 
-                new Subcategoria("Elmos", Arrays.asList(
-
+                new Subcategoria("Elmo de Placa", Arrays.asList(
+                        item("HEAD_PLATE_SET1", "Elmo de Soldado", "elmo de soldado"),
+                        item("HEAD_PLATE_SET2", "Elmo de Cavaleiro", "elmo de cavaleiro"),
+                        item("HEAD_PLATE_SET3", "Elmo de Guardião", "elmo de guardiao"),
+                        item("HEAD_PLATE_UNDEAD", "Elmo de Guarda-tumbas", "elmo de guarda tumbas"),
+                        item("HEAD_PLATE_HELL", "Elmo Demônio", "elmo demonio"),
+                        item("HEAD_PLATE_KEEPER", "Elmo Judicante", "elmo judicante"),
+                        item("HEAD_PLATE_FEY", "Elmo de Tecelão do Crepúsculo", "elmo de tecelao do crepusculo"),
+                        item("HEAD_PLATE_AVALON", "Elmo da Bravura", "elmo da bravura"),
+                        item("HEAD_PLATE_ROYAL", "Elmo Real", "elmo real")
                 ))
         )));
 
         // peitoral
         lista.add(new Categoria("", "Armaduras de Peitoral", Arrays.asList(
-
-                new Subcategoria("Tecido (Mago)", Arrays.asList(
-
+                new Subcategoria("Armadura de Tecido", Arrays.asList(
+                        item("ARMOR_CLOTH_SET1", "Robe de Erudito", "robe de erudito"),
+                        item("ARMOR_CLOTH_SET2", "Robe de Clérigo", "robe de clerigo"),
+                        item("ARMOR_CLOTH_SET3", "Robe de Mago", "robe de mago"),
+                        item("ARMOR_CLOTH_KEEPER", "Robe de Druida", "robe de druida"),
+                        item("ARMOR_CLOTH_HELL", "Robe Malévolo", "robe de malevolo"),
+                        item("ARMOR_CLOTH_MORGANA", "Robe Sectário", "robe de sectario"),
+                        item("ARMOR_CLOTH_FEY", "Robe Feérico", "robe feerico"),
+                        item("ARMOR_CLOTH_AVALON", "Robe da Pureza", "robe da pureza"),
+                        item("ARMOR_CLOTH_ROYAL", "Robe Real", "robe real")
                 )),
 
-                new Subcategoria("Couro (Ranger)", Arrays.asList(
-
+                new Subcategoria("Armadura de Couro", Arrays.asList(
+                        item("ARMOR_LEATHER_SET1", "Casaco de Mercenário", "casaco de mercenario"),
+                        item("ARMOR_LEATHER_SET2", "Casaco de Caçador", "casaco de cacador caçador"),
+                        item("ARMOR_LEATHER_SET3", "Casaco de Assassino", "casaco de assassino"),
+                        item("RMOR_LEATHER_MORGANA", "Casaco de Espreitador", "casaco de espreitador"),
+                        item("ARMOR_LEATHER_HELL", "Casaco Inferial", "casaco inferial"),
+                        item("ARMOR_LEATHER_UNDEAD", "Casaco Espectral", "casaco espectral"),
+                        item("ARMOR_LEATHER_FEY", "Casaco de Andarilho da Névoa", "casaco de andarilho da nevoa"),
+                        item("", "Casaco da Tenacidade", "casaco da tenacidade")
                 )),
 
-                new Subcategoria("Placa (Guerreiro)", Arrays.asList(
-
+                new Subcategoria("Armadura de Placa", Arrays.asList(
+                        item("ARMOR_PLATE_SET1", "Armadura de Soldado", "armadura de soldado"),
+                        item("ARMOR_PLATE_SET2", "Armadura de Cavaleiro", "armadura de cavaleiro"),
+                        item("ARMOR_PLATE_SET3", "Armadura de Guardião", "armadura de guardiao"),
+                        item("ARMOR_PLATE_UNDEAD", "Armadura de Guarda-tumbas", "armadura de guarda tumbas"),
+                        item("ARMOR_PLATE_HELL", "Armadura Demônio", "armadura demonio"),
+                        item("ARMOR_PLATE_KEEPER", "Armadura Judicante", "armadura judicante"),
+                        item("ARMOR_PLATE_FEY", "Armadura de Tecelão do Crepúsculo", "armadura de tecelao do crepusculo"),
+                        item("ARMOR_PLATE_AVALON", "Armadura da Bravura", "armadura da bravura"),
+                        item("ARMOR_PLATE_ROYAL", "Armadura Real", "armadura real")
                 ))
         )));
+
+
 
         // botas e sapatos
         lista.add(new Categoria("", "Armadura de Calçado", Arrays.asList(
 
-                new Subcategoria("Tecido (Mago)", Arrays.asList(
 
-                        new ItemDefinition[]{})),
-
-                new Subcategoria("Couro (Ranger)", Arrays.asList(
-
+                new Subcategoria("Sapato de Tecido", Arrays.asList(
+                        item("SHOES_CLOTH_SET1", "Sandálias de Erudito", "sandálias de erudito"),
+                        item("SHOES_CLOTH_SET2", "Sandálias de Clérigo", "sandálias de clerigo"),
+                        item("SHOES_CLOTH_SET3", "Sandálias de Mago", "sandálias de mago"),
+                        item("SHOES_CLOTH_KEEPER", "Sandálias de Druida", "sandálias de druida"),
+                        item("SHOES_CLOTH_HELL", "Sandálias Malévolo", "sandálias de malevolo"),
+                        item("SHOES_CLOTH_MORGANA", "Sandálias Sectário", "sandálias de sectario"),
+                        item("SHOES_CLOTH_FEY", "Sandálias Feérico", "sandálias feerico"),
+                        item("SHOES_CLOTH_AVALON", "Sandálias da Pureza", "sandálias da pureza"),
+                        item("SHOES_CLOTH_ROYAL", "Sandálias Real", "sandálias real")
                 )),
 
-                new Subcategoria("Placa (Guerreiro)", Arrays.asList(
+                new Subcategoria("Sapato de Couro", Arrays.asList(
+                        item("SHOES_LEATHER_SET1", "Sapatos de Mercenário", "sapatos de mercenario"),
+                        item("SHOES_LEATHER_SET2", "Sapatos de Caçador", "sapatos de cacador caçador"),
+                        item("SHOES_LEATHER_SET3", "Sapatos de Assassino", "sapatos de assassino"),
+                        item("SHOES_LEATHER_MORGANA", "Sapatos de Espreitador", "sapatos de espreitador"),
+                        item("SHOES_LEATHER_HELL", "Sapatos Inferial", "sapatos inferial"),
+                        item("SHOES_LEATHER_UNDEAD", "Sapatos Espectral", "sapatos espectral"),
+                        item("SHOES_LEATHER_FEY", "Sapatos de Andarilho da Névoa", "sapatos de andarilho da nevoa"),
+                        item("SHOES_LEATHER_AVALON", "Sapatos da Tenacidade", "sapatos da tenacidade")
+                )),
 
+
+                new Subcategoria("Sapato de Placa", Arrays.asList(
+                        item("SHOES_PLATE_SET1", "Botas de Soldado", "botas de soldado"),
+                        item("SHOES_PLATE_SET2", "Botas de Cavaleiro", "botas de cavaleiro"),
+                        item("SHOES_PLATE_SET3", "Botas de Guardião", "botas de guardiao"),
+                        item("SHOES_PLATE_UNDEAD", "Botas de Guarda-tumbas", "botas de guarda tumbas"),
+                        item("SHOES_PLATE_HELL", "Botas Demônio", "botas demonio"),
+                        item("SHOES_PLATE_KEEPER", "Botas Judicante", "botas judicante"),
+                        item("SHOES_PLATE_FEY", "Botas de Tecelão do Crepúsculo", "botas de tecelao do crepusculo"),
+                        item("SHOES_PLATE_AVALON", "Botas da Bravura", "botas da bravura"),
+                        item("SHOES_PLATE_ROYAL", "Botas Real", "botas real")
                 ))
         )));
 
@@ -301,7 +434,6 @@ public class BancoDeDados {
 
 
         )));
-
 
 
         return lista;
