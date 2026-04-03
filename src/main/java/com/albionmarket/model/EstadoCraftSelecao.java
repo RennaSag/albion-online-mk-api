991c744f29fd5559eea02f90dc4637925bbb4c0f
Renna Sag <rennasamuel_2003@hotmail.com>
2026-04-03 02:41:10 -0300

    nada

--- src/main/java/com/albionmarket/model/EstadoCraftSelecao.java
+++ src/main/java/com/albionmarket/model/EstadoCraftSelecao.java
@@ -10,19 +10,8 @@ public class EstadoCraftSelecao {
     public final String textoBusca;
     public final List<String> cidades;
 
-    // construtor antigo sem cidades, para compatibilidade
-    /*
-    public EstadoCraftSelecao(ItemDefinition item, int tier, int enchant, String textoBusca) {
-        this.item       = item;
-        this.tier       = tier;
-        this.enchant    = enchant;
-        this.textoBusca = textoBusca;
-        this.cidades    = null;
-    }
-    */
 
-
-    // construtor novo com cidades
+    // construtor com cidades
     public EstadoCraftSelecao(ItemDefinition item, int tier, int enchant,
                               String textoBusca, List<String> cidades) {
         this.item = item;
