fa52a804e1082c91d60c041dacd1c9edb133f039
Renna Sag <rennasamuel_2003@hotmail.com>
2026-05-01 20:38:55 -0300

    TENTANDO RESOLVER EMAIL AUTOMATICO

--- licencas-server/index.js
+++ licencas-server/index.js
@@ -246,6 +246,7 @@ async function enviarEmailChave(email, chave) {
 
     const transporter = nodemailer.createTransport({
         service: 'gmail',
+        family: 4,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
