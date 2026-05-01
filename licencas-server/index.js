708eff200e908184d1a736d0ce5207841082ed6e
Renna Sag <rennasamuel_2003@hotmail.com>
2026-05-01 18:20:44 -0300

    TENTANDO RESOLVER EMAIL AUTOMATICO

--- licencas-server/index.js
+++ licencas-server/index.js
@@ -266,12 +266,17 @@ async function enviarEmailChave(email, chave) {
         host: 'smtp.gmail.com',
         port: 587,
         secure: false,
+        connectionTimeout: 10000,
+        greetingTimeout: 10000,
+        socketTimeout: 10000,
         family: 4,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
         }
     });
+
+    console.log('tentando enviar...');
 
     const info = await transporter.sendMail({
         from: process.env.EMAIL_USER,
