1e6e171f0c65377fb3d0732ac40d98b12cae1c82
Renna Sag <rennasamuel_2003@hotmail.com>
2026-05-01 17:35:55 -0300

    tentando arrumar o node.js

--- licencas-server/index.js
+++ licencas-server/index.js
@@ -264,8 +264,9 @@ async function enviarEmailChave(email, chave) {
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,
+        family: 4,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
