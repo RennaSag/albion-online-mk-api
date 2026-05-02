bb8757efc9a785a695e7b823f9b0fd676c47742e
Renna Sag <rennasamuel_2003@hotmail.com>
2026-05-02 19:59:33 -0300

    correcao para tela refino

--- src/main/java/com/albionmarket/service/BancoDeDadosChangelog.java
+++ src/main/java/com/albionmarket/service/BancoDeDadosChangelog.java
@@ -19,14 +19,14 @@ import java.util.List;
 public class BancoDeDadosChangelog {
 
 
-    public static final String APP_VERSAO_ATUAL = "1.0.7";
+    public static final String APP_VERSAO_ATUAL = "1.0.8";
 
 
     public static final List<VersaoInfo> VERSOES = Arrays.asList(
 
 
             new VersaoInfo(
-                    "1.0.7",
+                    "1.0.8",
                     "02 de Maio, 2026",
                     "Otimizações e correção de bugs",
                     Arrays.asList(
@@ -34,7 +34,8 @@ public class BancoDeDadosChangelog {
                             new Mudanca(TipoMudanca.CORRECAO, "Por conta de um problema no servidor, alguns usuários não estavam recebendo a key de ativação no momento da compra, e isso foi corrigido."),
                             new Mudanca(TipoMudanca.NOVIDADE, "Nas versões futuras, haverá melhorias no sistema de busca de dados pela api, com o proposito de deixar as requisições mais rápidas."),
                             new Mudanca(TipoMudanca.REMOCAO, "Preste atenção no craft com diários! O campo 'Lucro' na tabela de craft não está incluindo o lucro com a venda dos diários de craft."),
-                            new Mudanca(TipoMudanca.REMOCAO, "Se você está tendo ou tiver qualquer problema com o aplicativo, entre em contato comigo pelo email de suporte: rennasagcontato@gmail.com. Um forte abraço")
+                            new Mudanca(TipoMudanca.REMOCAO, "Se você está tendo ou tiver qualquer problema com o aplicativo, entre em contato comigo pelo email de suporte: rennasagcontato@gmail.com. Um forte abraço"),
+                            new Mudanca(TipoMudanca.CORRECAO, "Corrigido cálculo de refino, eu tinha mexido no código e ficou errado, mas agora ta resolvido")
 
                     )
             )
